8835d1ef14de037f27ae44f6b6e46078d4167df0
Hien Nguyen <nlhien@fossil.com>
2020-02-28 14:24:50 +0700

    add fs_protobuf specs

--- push-to-local-podspecs.sh
+++ push-to-local-podspecs.sh
@@ -1,4 +1,4 @@
-pod repo push misfit-podspecs 'Protobuf-C++.podspec' \
+pod repo push misfit-podspecs 'FS_Protobuf-C++.podspec' \
     --sources='git@bitbucket.org:misfitwearablesinc/misfit-podspecs.git,https://github.com/CocoaPods/Specs.git' \
     --allow-warnings \
     --skip-import-validation \
